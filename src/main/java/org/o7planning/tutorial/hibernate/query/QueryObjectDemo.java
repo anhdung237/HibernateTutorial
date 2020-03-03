b812336c531d1d00f748f5c6154aa09b074e505b
AAVN\padung <anhdung237@gmail.com>
2020-03-03 17:53:12 +0700

    Can all QueryObjectDemo, example 1

--- src/main/java/org/o7planning/tutorial/hibernate/query/QueryObjectDemo.java
+++ src/main/java/org/o7planning/tutorial/hibernate/query/QueryObjectDemo.java
@@ -2,12 +2,12 @@ package org.o7planning.tutorial.hibernate.query;
 
 import java.util.List;
 
-import org.hibernate.Hibernate;
-import org.hibernate.Query;
+import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.o7planning.tutorial.hibernate.HibernateUtils;
 import org.o7planning.tutorial.hibernate.entities.Employee;
-import org.o7planning.tutorial.hibernate.entities.HibernateUtils;
+
 
 public class QueryObjectDemo {
 	public static void main(String[] args){
@@ -25,7 +25,10 @@ public class QueryObjectDemo {
 			//Tao mot cau lenh HQL query object
 			// tuong duong voi native Sql
 			// select * from employee e order by e.emp_name, e.emp_no
-			String sql = "select e from " + Employee.class.getName() + "e " + "order by e.empName, e.empNo";
+//			String sql = "select e from " + Employee.class.getName() + " e "	
+//	                  + " order by e.empName, e.empNo ";
+			
+			String sql = "select e from " + "Employee" + " e " + "order by e.empName, e.empNo";
 			
 			// Tao doi tuong Query
 			Query<Employee> query = session.createQuery(sql);
@@ -50,3 +53,56 @@ public class QueryObjectDemo {
 	
 	}
 }
+
+
+//package org.o7planning.tutorial.hibernate.query;
+// 
+//import java.util.List;
+// 
+//import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.o7planning.tutorial.hibernate.HibernateUtils;
+//import org.o7planning.tutorial.hibernate.entities.Employee;
+// 
+//public class QueryObjectDemo {
+// 
+//  public static void main(String[] args) {
+//      SessionFactory factory = HibernateUtils.getSessionFactory();
+// 
+//      Session session = factory.getCurrentSession();
+// 
+//      try {
+//          // Tất cả các lệnh hành động với DB thông qua Hibernate
+//          // đều phải nằm trong 1 giao dịch (Transaction)
+//          // Bắt đầu giao dịch
+//          session.getTransaction().begin();
+// 
+//          // Tạo một câu lệnh HQL query object.
+//          // Tương đương với Native SQL:
+//          // Select e.* from EMPLOYEE e order by e.EMP_NAME, e.EMP_NO
+// 
+//          String sql = "Select e from " + Employee.class.getName() + " e "
+//                  + " order by e.empName, e.empNo ";
+// 
+//          // Tạo đối tượng Query.
+//          Query<Employee> query = session.createQuery(sql);
+// 
+//          // Thực hiện truy vấn.
+//          List<Employee> employees = query.getResultList();
+// 
+//          for (Employee emp : employees) {
+//              System.out.println("Emp: " + emp.getEmpNo() + " : "
+//                      + emp.getEmpName());
+//          }
+// 
+//          // Commit dữ liệu
+//          session.getTransaction().commit();
+//      } catch (Exception e) {
+//          e.printStackTrace();
+//          // Rollback trong trường hợp có lỗi xẩy ra.
+//          session.getTransaction().rollback();
+//      }
+//  }
+//  
+//}
